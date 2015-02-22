fb88788432d423d076a15589814d310daad53b16
kodumen <r.b.daitan@gmail.com>
2015-02-22 14:40:30 +0800

    Added project.

---
+++ Kalkalator/Form1.Designer.vb
@@ -0,0 +1,444 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Textbox_Disp = New System.Windows.Forms.TextBox()
+        Me.Textbox_Hist = New System.Windows.Forms.TextBox()
+        Me.Btn_MC = New System.Windows.Forms.Button()
+        Me.Btn_MR = New System.Windows.Forms.Button()
+        Me.Btn_MS = New System.Windows.Forms.Button()
+        Me.Btn_Mplus = New System.Windows.Forms.Button()
+        Me.Btn_Mminus = New System.Windows.Forms.Button()
+        Me.Btn_Sqrt = New System.Windows.Forms.Button()
+        Me.Btn_Sign = New System.Windows.Forms.Button()
+        Me.Btn_C = New System.Windows.Forms.Button()
+        Me.Btn_CE = New System.Windows.Forms.Button()
+        Me.Btn_Backspace = New System.Windows.Forms.Button()
+        Me.Btn_Percent = New System.Windows.Forms.Button()
+        Me.Btn_Divide = New System.Windows.Forms.Button()
+        Me.Btn_9 = New System.Windows.Forms.Button()
+        Me.Btn_8 = New System.Windows.Forms.Button()
+        Me.Btn_7 = New System.Windows.Forms.Button()
+        Me.Btn_Reciprocal = New System.Windows.Forms.Button()
+        Me.Btn_Multiply = New System.Windows.Forms.Button()
+        Me.Btn_6 = New System.Windows.Forms.Button()
+        Me.Btn_5 = New System.Windows.Forms.Button()
+        Me.Btn_4 = New System.Windows.Forms.Button()
+        Me.Btn_3 = New System.Windows.Forms.Button()
+        Me.Btn_2 = New System.Windows.Forms.Button()
+        Me.Btn_1 = New System.Windows.Forms.Button()
+        Me.Btn_Minus = New System.Windows.Forms.Button()
+        Me.Btn_0 = New System.Windows.Forms.Button()
+        Me.Btn_Point = New System.Windows.Forms.Button()
+        Me.Btn_Plus = New System.Windows.Forms.Button()
+        Me.Btn_Equals = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Textbox_Disp
+        '
+        Me.Textbox_Disp.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Textbox_Disp.Cursor = System.Windows.Forms.Cursors.No
+        Me.Textbox_Disp.Enabled = False
+        Me.Textbox_Disp.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Textbox_Disp.Location = New System.Drawing.Point(12, 32)
+        Me.Textbox_Disp.Margin = New System.Windows.Forms.Padding(3, 0, 3, 3)
+        Me.Textbox_Disp.Name = "Textbox_Disp"
+        Me.Textbox_Disp.ReadOnly = True
+        Me.Textbox_Disp.ShortcutsEnabled = False
+        Me.Textbox_Disp.Size = New System.Drawing.Size(231, 38)
+        Me.Textbox_Disp.TabIndex = 1
+        Me.Textbox_Disp.TabStop = False
+        Me.Textbox_Disp.Text = "0"
+        Me.Textbox_Disp.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Textbox_Hist
+        '
+        Me.Textbox_Hist.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Textbox_Hist.Cursor = System.Windows.Forms.Cursors.No
+        Me.Textbox_Hist.Enabled = False
+        Me.Textbox_Hist.Location = New System.Drawing.Point(12, 12)
+        Me.Textbox_Hist.Margin = New System.Windows.Forms.Padding(3, 3, 3, 0)
+        Me.Textbox_Hist.Name = "Textbox_Hist"
+        Me.Textbox_Hist.ReadOnly = True
+        Me.Textbox_Hist.ShortcutsEnabled = False
+        Me.Textbox_Hist.Size = New System.Drawing.Size(231, 20)
+        Me.Textbox_Hist.TabIndex = 2
+        Me.Textbox_Hist.TabStop = False
+        Me.Textbox_Hist.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Btn_MC
+        '
+        Me.Btn_MC.Location = New System.Drawing.Point(12, 85)
+        Me.Btn_MC.Name = "Btn_MC"
+        Me.Btn_MC.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_MC.TabIndex = 3
+        Me.Btn_MC.TabStop = False
+        Me.Btn_MC.Text = "MC"
+        Me.Btn_MC.UseVisualStyleBackColor = True
+        '
+        'Btn_MR
+        '
+        Me.Btn_MR.Location = New System.Drawing.Point(60, 85)
+        Me.Btn_MR.Name = "Btn_MR"
+        Me.Btn_MR.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_MR.TabIndex = 4
+        Me.Btn_MR.TabStop = False
+        Me.Btn_MR.Text = "MR"
+        Me.Btn_MR.UseVisualStyleBackColor = True
+        '
+        'Btn_MS
+        '
+        Me.Btn_MS.Location = New System.Drawing.Point(108, 85)
+        Me.Btn_MS.Name = "Btn_MS"
+        Me.Btn_MS.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_MS.TabIndex = 5
+        Me.Btn_MS.TabStop = False
+        Me.Btn_MS.Text = "MS"
+        Me.Btn_MS.UseVisualStyleBackColor = True
+        '
+        'Btn_Mplus
+        '
+        Me.Btn_Mplus.Location = New System.Drawing.Point(156, 85)
+        Me.Btn_Mplus.Name = "Btn_Mplus"
+        Me.Btn_Mplus.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_Mplus.TabIndex = 6
+        Me.Btn_Mplus.TabStop = False
+        Me.Btn_Mplus.Text = "M+"
+        Me.Btn_Mplus.UseVisualStyleBackColor = True
+        '
+        'Btn_Mminus
+        '
+        Me.Btn_Mminus.Location = New System.Drawing.Point(204, 85)
+        Me.Btn_Mminus.Name = "Btn_Mminus"
+        Me.Btn_Mminus.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_Mminus.TabIndex = 7
+        Me.Btn_Mminus.TabStop = False
+        Me.Btn_Mminus.Text = "M-"
+        Me.Btn_Mminus.UseVisualStyleBackColor = True
+        '
+        'Btn_Sqrt
+        '
+        Me.Btn_Sqrt.Location = New System.Drawing.Point(204, 119)
+        Me.Btn_Sqrt.Name = "Btn_Sqrt"
+        Me.Btn_Sqrt.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_Sqrt.TabIndex = 12
+        Me.Btn_Sqrt.TabStop = False
+        Me.Btn_Sqrt.Text = "sqrt"
+        Me.Btn_Sqrt.UseVisualStyleBackColor = True
+        '
+        'Btn_Sign
+        '
+        Me.Btn_Sign.Location = New System.Drawing.Point(156, 119)
+        Me.Btn_Sign.Name = "Btn_Sign"
+        Me.Btn_Sign.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_Sign.TabIndex = 11
+        Me.Btn_Sign.TabStop = False
+        Me.Btn_Sign.Text = "+/-"
+        Me.Btn_Sign.UseVisualStyleBackColor = True
+        '
+        'Btn_C
+        '
+        Me.Btn_C.Location = New System.Drawing.Point(108, 119)
+        Me.Btn_C.Name = "Btn_C"
+        Me.Btn_C.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_C.TabIndex = 10
+        Me.Btn_C.TabStop = False
+        Me.Btn_C.Text = "C"
+        Me.Btn_C.UseVisualStyleBackColor = True
+        '
+        'Btn_CE
+        '
+        Me.Btn_CE.Location = New System.Drawing.Point(60, 119)
+        Me.Btn_CE.Name = "Btn_CE"
+        Me.Btn_CE.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_CE.TabIndex = 9
+        Me.Btn_CE.TabStop = False
+        Me.Btn_CE.Text = "CE"
+        Me.Btn_CE.UseVisualStyleBackColor = True
+        '
+        'Btn_Backspace
+        '
+        Me.Btn_Backspace.Location = New System.Drawing.Point(12, 119)
+        Me.Btn_Backspace.Name = "Btn_Backspace"
+        Me.Btn_Backspace.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_Backspace.TabIndex = 8
+        Me.Btn_Backspace.TabStop = False
+        Me.Btn_Backspace.Text = "<--"
+        Me.Btn_Backspace.UseVisualStyleBackColor = True
+        '
+        'Btn_Percent
+        '
+        Me.Btn_Percent.Location = New System.Drawing.Point(204, 153)
+        Me.Btn_Percent.Name = "Btn_Percent"
+        Me.Btn_Percent.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_Percent.TabIndex = 17
+        Me.Btn_Percent.TabStop = False
+        Me.Btn_Percent.Text = "%"
+        Me.Btn_Percent.UseVisualStyleBackColor = True
+        '
+        'Btn_Divide
+        '
+        Me.Btn_Divide.Location = New System.Drawing.Point(156, 153)
+        Me.Btn_Divide.Name = "Btn_Divide"
+        Me.Btn_Divide.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_Divide.TabIndex = 16
+        Me.Btn_Divide.TabStop = False
+        Me.Btn_Divide.Text = "/"
+        Me.Btn_Divide.UseVisualStyleBackColor = True
+        '
+        'Btn_9
+        '
+        Me.Btn_9.Location = New System.Drawing.Point(108, 153)
+        Me.Btn_9.Name = "Btn_9"
+        Me.Btn_9.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_9.TabIndex = 15
+        Me.Btn_9.TabStop = False
+        Me.Btn_9.Text = "9"
+        Me.Btn_9.UseVisualStyleBackColor = True
+        '
+        'Btn_8
+        '
+        Me.Btn_8.Location = New System.Drawing.Point(60, 153)
+        Me.Btn_8.Name = "Btn_8"
+        Me.Btn_8.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_8.TabIndex = 14
+        Me.Btn_8.TabStop = False
+        Me.Btn_8.Text = "8"
+        Me.Btn_8.UseVisualStyleBackColor = True
+        '
+        'Btn_7
+        '
+        Me.Btn_7.Location = New System.Drawing.Point(12, 153)
+        Me.Btn_7.Name = "Btn_7"
+        Me.Btn_7.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_7.TabIndex = 13
+        Me.Btn_7.TabStop = False
+        Me.Btn_7.Text = "7"
+        Me.Btn_7.UseVisualStyleBackColor = True
+        '
+        'Btn_Reciprocal
+        '
+        Me.Btn_Reciprocal.Location = New System.Drawing.Point(204, 187)
+        Me.Btn_Reciprocal.Name = "Btn_Reciprocal"
+        Me.Btn_Reciprocal.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_Reciprocal.TabIndex = 22
+        Me.Btn_Reciprocal.TabStop = False
+        Me.Btn_Reciprocal.Text = "1/x"
+        Me.Btn_Reciprocal.UseVisualStyleBackColor = True
+        '
+        'Btn_Multiply
+        '
+        Me.Btn_Multiply.Location = New System.Drawing.Point(156, 187)
+        Me.Btn_Multiply.Name = "Btn_Multiply"
+        Me.Btn_Multiply.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_Multiply.TabIndex = 21
+        Me.Btn_Multiply.TabStop = False
+        Me.Btn_Multiply.Text = "*"
+        Me.Btn_Multiply.UseVisualStyleBackColor = True
+        '
+        'Btn_6
+        '
+        Me.Btn_6.Location = New System.Drawing.Point(108, 187)
+        Me.Btn_6.Name = "Btn_6"
+        Me.Btn_6.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_6.TabIndex = 20
+        Me.Btn_6.TabStop = False
+        Me.Btn_6.Text = "6"
+        Me.Btn_6.UseVisualStyleBackColor = True
+        '
+        'Btn_5
+        '
+        Me.Btn_5.Location = New System.Drawing.Point(60, 187)
+        Me.Btn_5.Name = "Btn_5"
+        Me.Btn_5.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_5.TabIndex = 19
+        Me.Btn_5.TabStop = False
+        Me.Btn_5.Text = "5"
+        Me.Btn_5.UseVisualStyleBackColor = True
+        '
+        'Btn_4
+        '
+        Me.Btn_4.Location = New System.Drawing.Point(12, 187)
+        Me.Btn_4.Name = "Btn_4"
+        Me.Btn_4.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_4.TabIndex = 18
+        Me.Btn_4.TabStop = False
+        Me.Btn_4.Text = "4"
+        Me.Btn_4.UseVisualStyleBackColor = True
+        '
+        'Btn_3
+        '
+        Me.Btn_3.Location = New System.Drawing.Point(108, 221)
+        Me.Btn_3.Name = "Btn_3"
+        Me.Btn_3.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_3.TabIndex = 25
+        Me.Btn_3.TabStop = False
+        Me.Btn_3.Text = "3"
+        Me.Btn_3.UseVisualStyleBackColor = True
+        '
+        'Btn_2
+        '
+        Me.Btn_2.Location = New System.Drawing.Point(60, 221)
+        Me.Btn_2.Name = "Btn_2"
+        Me.Btn_2.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_2.TabIndex = 24
+        Me.Btn_2.TabStop = False
+        Me.Btn_2.Text = "2"
+        Me.Btn_2.UseVisualStyleBackColor = True
+        '
+        'Btn_1
+        '
+        Me.Btn_1.Location = New System.Drawing.Point(12, 221)
+        Me.Btn_1.Name = "Btn_1"
+        Me.Btn_1.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_1.TabIndex = 23
+        Me.Btn_1.TabStop = False
+        Me.Btn_1.Text = "1"
+        Me.Btn_1.UseVisualStyleBackColor = True
+        '
+        'Btn_Minus
+        '
+        Me.Btn_Minus.Location = New System.Drawing.Point(156, 221)
+        Me.Btn_Minus.Name = "Btn_Minus"
+        Me.Btn_Minus.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_Minus.TabIndex = 26
+        Me.Btn_Minus.TabStop = False
+        Me.Btn_Minus.Text = "-"
+        Me.Btn_Minus.UseVisualStyleBackColor = True
+        '
+        'Btn_0
+        '
+        Me.Btn_0.Location = New System.Drawing.Point(12, 255)
+        Me.Btn_0.Name = "Btn_0"
+        Me.Btn_0.Size = New System.Drawing.Size(87, 28)
+        Me.Btn_0.TabIndex = 27
+        Me.Btn_0.TabStop = False
+        Me.Btn_0.Text = "0"
+        Me.Btn_0.UseVisualStyleBackColor = True
+        '
+        'Btn_Point
+        '
+        Me.Btn_Point.Location = New System.Drawing.Point(108, 255)
+        Me.Btn_Point.Name = "Btn_Point"
+        Me.Btn_Point.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_Point.TabIndex = 28
+        Me.Btn_Point.TabStop = False
+        Me.Btn_Point.Text = "."
+        Me.Btn_Point.UseVisualStyleBackColor = True
+        '
+        'Btn_Plus
+        '
+        Me.Btn_Plus.Location = New System.Drawing.Point(156, 255)
+        Me.Btn_Plus.Name = "Btn_Plus"
+        Me.Btn_Plus.Size = New System.Drawing.Size(39, 28)
+        Me.Btn_Plus.TabIndex = 29
+        Me.Btn_Plus.TabStop = False
+        Me.Btn_Plus.Text = "+"
+        Me.Btn_Plus.UseVisualStyleBackColor = True
+        '
+        'Btn_Equals
+        '
+        Me.Btn_Equals.Location = New System.Drawing.Point(204, 221)
+        Me.Btn_Equals.Name = "Btn_Equals"
+        Me.Btn_Equals.Size = New System.Drawing.Size(39, 62)
+        Me.Btn_Equals.TabIndex = 30
+        Me.Btn_Equals.TabStop = False
+        Me.Btn_Equals.Text = "="
+        Me.Btn_Equals.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(256, 308)
+        Me.Controls.Add(Me.Btn_Equals)
+        Me.Controls.Add(Me.Btn_Plus)
+        Me.Controls.Add(Me.Btn_Point)
+        Me.Controls.Add(Me.Btn_0)
+        Me.Controls.Add(Me.Btn_Minus)
+        Me.Controls.Add(Me.Btn_3)
+        Me.Controls.Add(Me.Btn_2)
+        Me.Controls.Add(Me.Btn_1)
+        Me.Controls.Add(Me.Btn_Reciprocal)
+        Me.Controls.Add(Me.Btn_Multiply)
+        Me.Controls.Add(Me.Btn_6)
+        Me.Controls.Add(Me.Btn_5)
+        Me.Controls.Add(Me.Btn_4)
+        Me.Controls.Add(Me.Btn_Percent)
+        Me.Controls.Add(Me.Btn_Divide)
+        Me.Controls.Add(Me.Btn_9)
+        Me.Controls.Add(Me.Btn_8)
+        Me.Controls.Add(Me.Btn_7)
+        Me.Controls.Add(Me.Btn_Sqrt)
+        Me.Controls.Add(Me.Btn_Sign)
+        Me.Controls.Add(Me.Btn_C)
+        Me.Controls.Add(Me.Btn_CE)
+        Me.Controls.Add(Me.Btn_Backspace)
+        Me.Controls.Add(Me.Btn_Mminus)
+        Me.Controls.Add(Me.Btn_Mplus)
+        Me.Controls.Add(Me.Btn_MS)
+        Me.Controls.Add(Me.Btn_MR)
+        Me.Controls.Add(Me.Btn_MC)
+        Me.Controls.Add(Me.Textbox_Hist)
+        Me.Controls.Add(Me.Textbox_Disp)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Kalkalator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Textbox_Disp As System.Windows.Forms.TextBox
+    Friend WithEvents Textbox_Hist As System.Windows.Forms.TextBox
+    Friend WithEvents Btn_MC As System.Windows.Forms.Button
+    Friend WithEvents Btn_MR As System.Windows.Forms.Button
+    Friend WithEvents Btn_MS As System.Windows.Forms.Button
+    Friend WithEvents Btn_Mplus As System.Windows.Forms.Button
+    Friend WithEvents Btn_Mminus As System.Windows.Forms.Button
+    Friend WithEvents Btn_Sqrt As System.Windows.Forms.Button
+    Friend WithEvents Btn_Sign As System.Windows.Forms.Button
+    Friend WithEvents Btn_C As System.Windows.Forms.Button
+    Friend WithEvents Btn_CE As System.Windows.Forms.Button
+    Friend WithEvents Btn_Backspace As System.Windows.Forms.Button
+    Friend WithEvents Btn_Percent As System.Windows.Forms.Button
+    Friend WithEvents Btn_Divide As System.Windows.Forms.Button
+    Friend WithEvents Btn_9 As System.Windows.Forms.Button
+    Friend WithEvents Btn_8 As System.Windows.Forms.Button
+    Friend WithEvents Btn_7 As System.Windows.Forms.Button
+    Friend WithEvents Btn_Reciprocal As System.Windows.Forms.Button
+    Friend WithEvents Btn_Multiply As System.Windows.Forms.Button
+    Friend WithEvents Btn_6 As System.Windows.Forms.Button
+    Friend WithEvents Btn_5 As System.Windows.Forms.Button
+    Friend WithEvents Btn_4 As System.Windows.Forms.Button
+    Friend WithEvents Btn_3 As System.Windows.Forms.Button
+    Friend WithEvents Btn_2 As System.Windows.Forms.Button
+    Friend WithEvents Btn_1 As System.Windows.Forms.Button
+    Friend WithEvents Btn_Minus As System.Windows.Forms.Button
+    Friend WithEvents Btn_0 As System.Windows.Forms.Button
+    Friend WithEvents Btn_Point As System.Windows.Forms.Button
+    Friend WithEvents Btn_Plus As System.Windows.Forms.Button
+    Friend WithEvents Btn_Equals As System.Windows.Forms.Button
+
+End Class
