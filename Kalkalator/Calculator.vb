dfee01b5dfa208827b7a392d04b3a2cc89a2cf6c
kodumen <r.b.daitan@gmail.com>
2015-02-23 17:55:12 +0800

    Restarting from empty class.

--- Kalkalator/Calculator.vb
+++ Kalkalator/Calculator.vb
@@ -2,11 +2,5 @@
 ' other calculator functions such as displaying text.
 
 Public Class Calculator
-    Dim input0 As Double
-    Dim input1 As Double
-    Dim in0isEmpty As Boolean   ' Is a number stored?
-    Dim in1isEmpty As Boolean
-    Dim answer As Double        ' Store answer here
-
 
 End Class
