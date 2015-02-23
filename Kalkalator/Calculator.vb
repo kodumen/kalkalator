d586a039f9e02efb961b51b11f6563877de05789
kodumen <r.b.daitan@gmail.com>
2015-02-23 18:27:22 +0800

    Added method to solve.

--- Kalkalator/Calculator.vb
+++ Kalkalator/Calculator.vb
@@ -21,9 +21,25 @@ Public Class Calculator
         If pushInput Then
             ' Solve input
             ' "Push" answer to input1
+            input1 = Solve()
+            pushInput = False
         End If
         Me.op = op
     End Sub
 
-    `
+    Public Function Solve() As Double
+        Dim answer As Double
+        If op.Equals(Calculator.Operation.Addition) Then
+            answer = input0 + input1
+        ElseIf op.Equals(Calculator.Operation.Subtraction) Then
+            answer = input0 - input1
+        ElseIf op.Equals(Calculator.Operation.Multiplication) Then
+            answer = input0 * input1
+        ElseIf op.Equals(Calculator.Operation.Division) Then
+            answer = input0 / input1
+        End If
+        Solve = answer
+    End Function
+
+
 End Class
