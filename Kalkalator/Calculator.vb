19b6c9eb5cdfe58d1fc13f2a5f79ce80d407dd94
kodumen <r.b.daitan@gmail.com>
2015-02-23 18:04:16 +0800

    Added input function.

--- Kalkalator/Calculator.vb
+++ Kalkalator/Calculator.vb
@@ -1,4 +1,9 @@
 ﻿' Does all the calculations.
 Public Class Calculator
+    Dim input As Double
+    Dim answer As Double
 
+    Public Sub SetInput(input As Double)
+        Me.input = input
+    End Sub
 End Class
